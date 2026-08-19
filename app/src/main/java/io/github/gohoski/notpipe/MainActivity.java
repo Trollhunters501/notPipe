08086464a927acf8ddcb849a3a28204473a45a95
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-19 14:36:01 -0600

    Add PopupMenu import to MainActivity

--- app/src/main/java/io/github/gohoski/notpipe/MainActivity.java
+++ app/src/main/java/io/github/gohoski/notpipe/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.PopupMenu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
